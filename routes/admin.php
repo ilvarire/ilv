5019cd7ac39a7df42819b784ccf4a01882809972
Your Name <you@example.com>
2025-11-30 13:45:21 -0800

    try

--- routes/admin.php
+++ routes/admin.php
@@ -27,9 +27,12 @@ use App\Livewire\Admin\Tags;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'rolemanager:admin'])->group(function () {
+    Route::get('/admin', Dashboard::class);
+});
+Route::middleware(['auth', 'rolemanager:admin'])->group(function () {
     Route::group(['prefix' => 'admin'], function () {
         //dashboard
-        Route::redirect('/', '/dashboard');
+        // Route::redirect('/', '/dashboard');
         Route::get('/dashboard', Dashboard::class)->name('admin.dashboard');
 
         //categories
